74a7dc022ff1a2d55f100d52f190c3559ad5ccb2
Doring <hojun.dev@gmail.com>
2020-12-06 20:33:51 +0900

    main() 파일
    개발중!

---
+++ C/CBattle.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "CBattleHead.h"
+//https://eskeptor.tistory.com/25 庆歹颇老 包府 规过
+
+int main() {
+	int turn_num = 0;
+
+	UserInfoSetup();
+	while (1) {
+		turn_num++;
+		BattleTurnStartMessage(turn_num);
+		SetCondition();
+		UserAct();
+	}
+}
